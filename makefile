2b1f4682349b660259e0a559a4eeaa37e51db1c5
isumalatov <isumalatov@gmail.com>
2024-03-22 23:35:07 +0100

    finish :)

---
+++ makefile
@@ -0,0 +1,13 @@
+#Ilyas Umalatov
+
+all: mcp
+
+maze: mcp.cc
+	g++ -o mcp mcp.cc
+
+tar:
+	tar -cvzf X727815E.tar.gz mcp.cc makefile
+
+clean:
+	rm -f mcp X727815E.tar.gz
+
--- mcp.cc
+++ mcp.cc
@@ -1,6 +1,8 @@
+//Ilyas Umalatov
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
@@ -12,14 +14,28 @@ int mcp_naive(const vector<vector<int>> &matrix, int posx, int posy, int rows, i
     }
     else
     {
-        int solright = mcp_naive(matrix, posx, posy + 1, rows, cols);
-        int soldown = mcp_naive(matrix, posx + 1, posy, rows, cols);
-        int soldiag = mcp_naive(matrix, posx + 1, posy + 1, rows, cols);
+        int solright = INT_MAX;
+        int soldown = INT_MAX;
+        int soldiag = INT_MAX;
+
+        if (posy + 1 < cols)
+        {
+            solright = mcp_naive(matrix, posx, posy + 1, rows, cols);
+        }
+        if (posx + 1 < rows)
+        {
+            soldown = mcp_naive(matrix, posx + 1, posy, rows, cols);
+        }
+        if (posx + 1 < rows && posy + 1 < cols)
+        {
+            soldiag = mcp_naive(matrix, posx + 1, posy + 1, rows, cols);
+        }
+
         return matrix[posx][posy] + min(solright, min(soldown, soldiag));
     }
 }
 
-int mcp_memo(const vector<vector<int>> &matrix, int posx, int posy, int rows, int cols, vector<vector<int>> &memo, vector<pair<int, int>> &path)
+int mcp_memo(const vector<vector<int>> &matrix, int posx, int posy, int rows, int cols, vector<vector<int>> &memo)
 {
     if (posx == rows - 1 && posy == cols - 1)
     {
@@ -31,25 +47,55 @@ int mcp_memo(const vector<vector<int>> &matrix, int posx, int posy, int rows, in
     }
     else
     {
-        int solright = mcp_memo(matrix, posx, posy + 1, rows, cols, memo, path);
-        int soldown = mcp_memo(matrix, posx + 1, posy, rows, cols, memo, path);
-        int soldiag = mcp_memo(matrix, posx + 1, posy + 1, rows, cols, memo, path);
+        int solright = INT_MAX;
+        int soldown = INT_MAX;
+        int soldiag = INT_MAX;
+
+        if (posy + 1 < cols)
+        {
+            solright = mcp_memo(matrix, posx, posy + 1, rows, cols, memo);
+        }
+        if (posx + 1 < rows)
+        {
+            soldown = mcp_memo(matrix, posx + 1, posy, rows, cols, memo);
+        }
+        if (posx + 1 < rows && posy + 1 < cols)
+        {
+            soldiag = mcp_memo(matrix, posx + 1, posy + 1, rows, cols, memo);
+        }
+
         int minimo = matrix[posx][posy] + min(solright, min(soldown, soldiag));
         memo[posx][posy] = minimo;
-        if (minimo == solright)
-        {
-            path.push_back({posx, posy + 1});
-        }
-        else if (minimo == soldown)
-        {
-            path.push_back({posx + 1, posy});
-        }
-        else
-        {
-            path.push_back({posx + 1, posy + 1});
-        }
+
         return minimo;
     }
+}
+
+vector<pair<int, int>> reconstruct_path(const vector<vector<int>> &memo, int rows, int cols) {
+    vector<pair<int, int>> path;
+    int x = 0, y = 0;
+
+    while (x < rows - 1 || y < cols - 1) {
+        path.push_back({x, y});
+
+        if (x + 1 < rows && y + 1 < cols) {
+            if (memo[x + 1][y] < memo[x][y + 1] && memo[x + 1][y] < memo[x + 1][y + 1]) {
+                x++;
+            } else if (memo[x][y + 1] < memo[x + 1][y + 1]) {
+                y++;
+            } else {
+                x++;
+                y++;
+            }
+        } else if (x + 1 < rows) {
+            x++;
+        } else {
+            y++;
+        }
+    }
+
+    path.push_back({rows - 1, cols - 1});
+    return path;
 }
 
 int mcp_it_matix()
@@ -62,9 +108,39 @@ int mcp_it_vector()
     return -1;
 }
 
-int mcp_parser()
+void mcp_parser(int rows, int cols, const vector<vector<int>> &matrix, const vector<vector<int>> &memo)
 {
-    return -1;
+    int coste = 0;
+    vector<vector<char>> charMatrix(rows, vector<char>(cols, '.'));
+
+    vector<pair<int, int>> path = reconstruct_path(memo, rows, cols);
+
+    for (int i = 0; i < path.size(); ++i)
+    {
+        charMatrix[path[i].first][path[i].second] = 'x';
+    }
+
+
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            cout << charMatrix[i][j];
+        }
+        cout << '\n';
+    }
+
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            if (charMatrix[i][j] == 'x')
+            {
+                coste += matrix[i][j];
+            }
+        }
+    }
+    cout << coste << endl;
 }
 
 int main(int argc, char *argv[])
@@ -74,34 +150,41 @@ int main(int argc, char *argv[])
     bool ignore_naive = false;
     if (argc < 3)
     {
-        cout << "Por favor, proporciona el nombre del archivo como argumento.\n";
+        cerr << "ERROR: missing filename.\nUsage:\nmcp [--p2D] [-t] [--ignore-naive] -f file" << endl;
         return 1;
     }
 
     ifstream file;
-    for (int i = 0; i < argc; ++i)
+    for (int i = 1; i < argc; ++i)
     {
-        if (argv[i] == "-t")
+        string arg = argv[i]; // Convert to std::string
+        if (arg == "-t")
         {
             t = true;
         }
-        else if (argv[i] == "--p2D")
+        else if (arg == "--p2D")
         {
             p2D = true;
         }
-        else if (argv[i] == "--ignore-naive")
+        else if (arg == "--ignore-naive")
         {
             ignore_naive = true;
         }
-        else if (argv[i] == "-f")
+        else if (arg == "-f")
         {
-            ifstream file(argv[i + 1]);
+            file.open(argv[i + 1]);
+            i++;
+        }
+        else
+        {
+            cerr << "ERROR: unknown option " << arg << ".\nUsage:\nmcp [--p2D] [-t] [--ignore-naive] -f file" << endl;
+            return 1;
         }
     }
 
     if (!file)
     {
-        cout << "No se pudo abrir el archivo.\n";
+        cerr << "No se pudo abrir el archivo.\n";
         return 1;
     }
 
@@ -119,72 +202,35 @@ int main(int argc, char *argv[])
 
     vector<vector<int>> memo(rows, vector<int>(cols, -1));
 
-    vector<pair<int, int>> path;
-
     if (ignore_naive == true)
     {
         cout << "- ";
-        cout << mcp_memo(matrix, 0, 0, rows, cols, memo, path);
+        cout << mcp_memo(matrix, 0, 0, rows, cols, memo) << " ";
     }
     else
     {
         cout << mcp_naive(matrix, 0, 0, rows, cols) << " ";
-        cout << mcp_memo(matrix, 0, 0, rows, cols, memo, path);
+        cout << mcp_memo(matrix, 0, 0, rows, cols, memo) << " ";
     }
 
     if (mcp_it_matix() == -1)
     {
-        cout << " ? ";
+        cout << "? ";
     }
 
     if (mcp_it_vector() == -1)
     {
-        cout << "? ";
-    }
-
-    if (mcp_parser() == -1)
-    {
-        cout << "?";
+        cout << "?\n";
     }
 
     if (p2D == true)
     {
-        int coste = 0;
-        // Create a character matrix filled with '.'
-        vector<vector<char>> charMatrix(rows, vector<char>(cols, '.'));
-
-        // Replace the points in the path with '*'
-        for (const auto &p : path)
-        {
-            charMatrix[p.first][p.second] = '*';
-        }
-
-        // Print the character matrix
-        for (int i = 0; i < rows; ++i)
-        {
-            for (int j = 0; j < cols; ++j)
-            {
-                cout << charMatrix[i][j];
-            }
-            cout << '\n';
-        }
-
-        for (int i = 0; i < rows; ++i)
-        {
-            for (int j = 0; j < cols; ++j)
-            {
-                if (charMatrix[i][j] == '*')
-                {
-                    coste += matrix[i][j];
-                }
-            }
-        }
-        cout << coste << endl;
+        mcp_parser(rows, cols, matrix, memo);
     }
 
     if (t == true)
     {
-        cout << "?";
+        cout << "?" << endl;
     }
 
     file.close();
